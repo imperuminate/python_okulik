d1478d4d5eba30ac6e5907a8b0d0e40ca96607c8
imperuminate <imperuminate@gmail.com>
2025-03-02 20:58:12 -0700

    Lessons 14 and 15 finished

--- python_jupiter_notes/Lesson_14_and_15/news.py
+++ python_jupiter_notes/Lesson_14_and_15/news.py
@@ -1,5 +1,6 @@
 num = 34
 
+
 while True:
     user_num = int(input("User num:"))
     if num == user_num:
@@ -9,3 +10,22 @@ while True:
     else:
         print("Enter lower number")
 
+
+while (user_num := int(input("User num:"))) != num:
+    if num > user_num:
+        print("Enter highter number")
+    else:
+        print("Enter lower number")
+
+
+while True:
+    user_input = input("Say something:")
+    match user_input:
+        case "Hi":
+            print("Hello")
+        case "":
+            print("You nead to say something")
+        case "Buy":
+            break
+        case _:
+            print("??")
